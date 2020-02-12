adc247127115c37fe874bf9e84bed2b14d3d054a
memsenpai <memsenpai3@gmail.com>
2020-02-12 22:18:19 +0700

    update Sett

--- DaoHungAIO/Champions/Sett.cs
+++ DaoHungAIO/Champions/Sett.cs
@@ -11,39 +11,47 @@ using static EnsoulSharp.SDK.Geometry;
 using Utility = EnsoulSharp.SDK.Utility;
 using Menu = EnsoulSharp.SDK.MenuUI.Menu;
 using Color = System.Drawing.Color;
+using EnsoulSharp.SDK.MenuUI;
 
 namespace DaoHungAIO.Champions
 {
     class Sett
     {
         private static Spell q, w, e, r;
-        private static Menu menu, combo, harass, misc, draw;
+        private static Menu menu, combo, harass, misc, ks, draw;
         private static AIHeroClient Player = ObjectManager.Player;
 
         #region
         private static readonly MenuBool Qcombo = new MenuBool("qcombo", "[Q] on Combo");
-        private static readonly MenuBool QAA = new MenuBool("QAA", "^ After auto attack", false);
+        private static readonly MenuSlider QAA = new MenuSlider("QAA", "Q Combo After x auto attack", 2, 0, 2);
         private static readonly MenuBool Wcombo = new MenuBool("wcombo", "[W] on Combo");
         private static readonly MenuSlider Wminimum = new MenuSlider("Wminimum", "^ minimum % mana for use(0 = disable)");
         private static readonly MenuBool Ecombo = new MenuBool("ecombo", "[E] on Combo");
-        private static readonly MenuBool Estun = new MenuBool("Estun", "^ must stun");
+        private static readonly MenuBool Estun = new MenuBool("Estun", "^ must stun", false);
         private static readonly MenuBool Rcombo = new MenuBool("Rcombo", "[R] on Combo");
         private static readonly MenuSlider RcomboMinHit = new MenuSlider("RcomboMinHit", "^ minimum hit", 0, 0, 4);
+        private static readonly MenuBool RFlash = new MenuBool("RFlash", "R Flash ?(Middle mouse for toggle)", false);
 
         private static readonly MenuBool Qharass = new MenuBool("qharass", "[Q] on Harass");
-        private static readonly MenuBool QAAH = new MenuBool("QAAH", "^ After auto attack", false);
+        private static readonly MenuSlider QAAH = new MenuSlider("QAAH", "^ After auto attack", 2, 0, 2);
         private static readonly MenuBool Wharass = new MenuBool("Wharass", "[W] on Harass");
         private static readonly MenuBool Eharass = new MenuBool("eharass", "[E] on Harass");
 
-        private static readonly MenuSlider MiscAutoW = new MenuSlider("MiscAutoW", "Auto W when mana higher than", 95);
+        private static readonly MenuSlider MiscAutoW = new MenuSlider("MiscAutoW", "Auto W when HP lower than(0 = disable)", 20);
         private static readonly MenuBool MiscEDash = new MenuBool("MiscEDash", "Auto E on Dash");
         private static readonly MenuBool MiscECC = new MenuBool("MiscECC", "Auto E On CC");
         private static readonly MenuBool MiscQOnE = new MenuBool("MiscQOnE", "Auto Q if E hit");
+
+        private static readonly MenuBool ksR = new MenuBool("ksR", "Use R");
 
 
         private static readonly MenuBool DrawW = new MenuBool("DrawW", "W range");
         private static readonly MenuBool DrawE = new MenuBool("DrawE", "E range");
         private static readonly MenuBool DrawR = new MenuBool("DrawR", "R range");
+        private static readonly MenuBool DrawREnd = new MenuBool("DrawREnd", "R End Position");
+
+        private int lastPunch = 3;
+        private AIHeroClient useRNow = null;
         private static readonly List<BuffType> CCList = new List<BuffType>() { BuffType.Blind, BuffType.Fear, BuffType.Knockback, BuffType.Knockup, BuffType.Sleep, BuffType.Stun, BuffType.Taunt, BuffType.Suppression, BuffType.Slow };
         #endregion
         public Sett()
@@ -60,39 +68,94 @@ namespace DaoHungAIO.Champions
             menu = new Menu("Sett", "DH.Sett", true);
             combo = new Menu("Combo", "Combo");
             harass = new Menu("Harass", "Harass");
+            misc = new Menu("Misc", "Misc");
+            ks = new Menu("KS", "KS");
             draw = new Menu("draw", "Draw");
 
             combo.Add(Qcombo);
-            combo.Add(QAA);
+            combo.Add(QAA).Permashow();
             combo.Add(Wcombo);
             combo.Add(Wminimum);
             combo.Add(Ecombo);
             combo.Add(Estun);
             combo.Add(Rcombo);
             combo.Add(RcomboMinHit);
+            combo.Add(RFlash).Permashow();
 
             harass.Add(Qharass);
             harass.Add(QAAH);
             harass.Add(Wharass);
             harass.Add(Eharass);
 
+            misc.Add(MiscAutoW);
+            misc.Add(MiscEDash);
 
             draw.Add(DrawW);
             draw.Add(DrawE);
             draw.Add(DrawR);
+            draw.Add(DrawREnd);
 
             menu.Add(combo);
             menu.Add(harass);
+            menu.Add(misc);
+            menu.Add(ks);
             menu.Add(draw);
             menu.Attach();
-
 
             Game.OnUpdate += OnTick;
             //Game.OnWndProc += OnWndProc;
 
-
+            Dash.OnDash += OnDash;
             Drawing.OnDraw += Drawing_OnDraw;
             Orbwalker.OnAction += OnAction;
+            AIHeroClient.OnProcessSpellCast += OnProcessSpellCast;
+            AIHeroClient.OnPlayAnimation += OnPlayAnimation;
+            Game.OnWndProc += Game_OnWndProc;
+        }
+        private static void Game_OnWndProc(GameWndProcEventArgs args)
+        {
+            if (args.Msg != (uint)WindowsMessages.MBUTTONDOWN)
+                return;
+
+            RFlash.Enabled = !RFlash.Enabled;
+        }
+        private void OnPlayAnimation(AIBaseClient sender, AIBaseClientPlayAnimationEventArgs args)
+        {
+            if (sender.IsMe)
+            {
+                if (args.Animation.Contains("Attack"))
+                {
+                    if (args.Animation.Contains("Passive")) {
+                        lastPunch = 2;
+                    } else
+                    {
+                        lastPunch = 1;
+                    }
+                }
+            }
+        }
+
+        private void OnProcessSpellCast(AIBaseClient sender, AIBaseClientProcessSpellCastEventArgs args)
+        {
+            if(sender.IsMe)
+            {
+
+                if(args.SData.Name == "summonerflash" && useRNow != null) {
+                    Utility.DelayAction.Add(50, () => r.Cast(useRNow));
+                    useRNow = null;
+                }
+            }
+        }
+
+        private void OnDash(AIBaseClient sender, Dash.DashArgs args)
+        {
+            if(sender is AIHeroClient && sender.IsValidEnemy())
+            {
+                if (MiscEDash.Enabled && sender.IsValidTarget(e.Range))
+                {
+                    e.Cast(sender);
+                }
+            }
         }
 
         private void OnAction(object sender, OrbwalkerActionArgs args)
@@ -101,7 +164,7 @@ namespace DaoHungAIO.Champions
             {
                 if(Orbwalker.ActiveMode == OrbwalkerMode.Harass)
                 {
-                    if (QAAH.Enabled && q.IsReady() && args.Target is AIHeroClient)
+                    if (QAAH.Value == lastPunch && q.IsReady() && args.Target is AIHeroClient)
                     {
                         q.Cast();
                         Orbwalker.ResetAutoAttackTimer();
@@ -109,7 +172,7 @@ namespace DaoHungAIO.Champions
                 }
                 if(Orbwalker.ActiveMode == OrbwalkerMode.Combo)
                 {
-                    if(QAA.Enabled && q.IsReady() && args.Target is AIHeroClient)
+                    if(lastPunch == QAA.Value && q.IsReady() && args.Target is AIHeroClient)
                     {
                         q.Cast();
                         Orbwalker.ResetAutoAttackTimer();
@@ -120,17 +183,44 @@ namespace DaoHungAIO.Champions
                
         private void OnTick(EventArgs args)
         {
-            switch (Orbwalker.ActiveMode)
+            if (!Player.IsDead)
             {
-                case (OrbwalkerMode.Combo):
-                    DoCombo();
-                    break;
-                case OrbwalkerMode.Harass:
-                    DoHarass();
-                    break;
+                switch (Orbwalker.ActiveMode)
+                {
+                    case (OrbwalkerMode.Combo):
+                        DoCombo();
+                        break;
+                    case OrbwalkerMode.Harass:
+                        DoHarass();
+                        break;
+                }
+            }
+
+            if (ksR.Enabled)
+            {
+                ksWithR();
             }
         }
 
+
+        private void ksWithR()
+        {
+            var targets = TargetSelector.GetTargets(400);
+            var canKill = targets.Where(t => r.GetDamage(t) > t.Health - 50).OrderByDescending(t => t.Experience).FirstOrDefault();
+            if(canKill != null)
+            {
+                r.Cast(canKill);
+            }
+            //else
+            //{
+            //    Dictionary<AIHeroClient, int> data = new Dictionary<AIHeroClient, int>();
+            //    targets.ForEach(t => {
+            //        data.Add(t, t.Position.Extend(Player.Position, -400).CountEnemyHeroesInRange(580));
+            //    });
+            //    var posHasTarget = data.Where(ele => ele.Value > 0).OrderByDescending(ele => ele.Value);
+            //    if()
+            //}
+        }
 
         private void DoHarass()
         {
@@ -158,7 +248,7 @@ namespace DaoHungAIO.Champions
             }
             if (target.IsValidTarget(q.Range) && Qharass.Enabled && q.IsReady())
             {
-                if(!QAAH.Enabled)
+                if(QAAH.Value== 0)
                 {
                     q.Cast();
                 }
@@ -168,9 +258,14 @@ namespace DaoHungAIO.Champions
         private void DoCombo()
         {
             var target = TargetSelector.SelectedTarget;
-            if (target == null || !target.IsValidTarget(e.Range) || target.HaveSpellShield())
+            var range = e.Range;
+            if (RFlash.Enabled)
             {
-                target = TargetSelector.GetTargets(e.Range).Where(t => !t.HaveSpellShield()).OrderByDescending(t => TargetSelector.GetPriority(t)).FirstOrDefault();
+                range = r.Range + 400;
+            }
+            if (target == null || !target.IsValidTarget(range) || target.HaveSpellShield())
+            {
+                target = TargetSelector.GetTargets(range).Where(t => !t.HaveSpellShield()).OrderByDescending(t => TargetSelector.GetPriority(t)).FirstOrDefault();
             }
 
             if (target == null)
@@ -192,16 +287,28 @@ namespace DaoHungAIO.Champions
                 }
                 
             }
-            if (target.IsValidTarget(200) &&Qcombo.Enabled && q.IsReady() && !QAA.Enabled)
+            if (target.IsValidTarget(200) && Qcombo.Enabled && q.IsReady() && QAA.Value == 0)
             {
                 q.Cast();
             }
-            if (target.IsValidTarget(r.Range) && Rcombo.Enabled && r.IsReady() && Rhit(target) >= RcomboMinHit.Value)
+            if (Rcombo.Enabled && r.IsReady() && Rhit(target) >= RcomboMinHit.Value)
             {
-                r.Cast(target);
+                if (target.IsValidTarget(r.Range))
+                {
+                    r.Cast(target);
+                } else if (RFlash && target.IsValidTarget(r.Range + 400))
+                {
+                    var flashSlot = Player.GetSpellSlot("summonerflash");
+                    if(Player.Spellbook.CanUseSpell(flashSlot) == SpellState.Ready)
+                    {
+                        Player.Spellbook.CastSpell(flashSlot, target.Position);
+                        useRNow = target;
+                    }
+
+                }
             }
 
-            if (w.IsReady() && Player.Mana >= Wminimum.Value)
+            if (w.IsReady() && (Player.ManaPercent >= Wminimum.Value || (Player.HealthPercent <= MiscAutoW.Value)))
             {
                 w.Cast(target);
             }
@@ -252,6 +359,13 @@ namespace DaoHungAIO.Champions
             if (DrawR.Enabled && r.IsReady())
             {
                 Render.Circle.DrawCircle(Player.Position, r.Range, Color.Pink, 1);
+            }
+
+            if(DrawREnd.Enabled && r.IsReady())
+            {
+                var target = TargetSelector.GetTarget(r.Range * 2);
+                var point = target.Position.Extend(Player.Position, -400);
+                Render.Circle.DrawCircle(point, 580, Color.LightPink, 1);
             }
         }
     }
